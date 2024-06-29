78b11cf4e429bc4165ee801b9c21bd29b929f1ba
barabnna <154731299+barabnna@users.noreply.github.com>
2024-06-30 01:43:52 +1000

    Update get_lectures.php

--- get_lectures.php
+++ get_lectures.php
@@ -1,9 +1,9 @@
 <?php
 // Подключение к базе данных
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "mydb";
+$username = "root";
+$password = "root";
+$dbname = "mydb.sql";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
