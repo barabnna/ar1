9497aa5c8a796b207369c6ac0e9582d27c5e1530
barabnna <154731299+barabnna@users.noreply.github.com>
2024-06-30 01:21:38 +1000

    Create update_lecture.php

---
+++ update_lecture.php
@@ -0,0 +1,27 @@
+<?php
+// Подключение к базе данных
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "mybd";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_error);
+}
+
+$id = $_POST['lecture_id'];
+$title = $_POST['lecture_title'];
+$content = $_POST['lecture_content'];
+
+$sql = "UPDATE lecture SET title_lecture='$title', text_lecture='$content' WHERE id_lecture=$id";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: index3.html"); // Перенаправление обратно на страницу управления лекциями
+} else {
+    echo "Error updating record: ". $conn->error;
+}
+
+$conn->close();
+?>
