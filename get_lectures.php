3fa5526e317b61233fdd83516e073af83ba841e1
barabnna <154731299+barabnna@users.noreply.github.com>
2024-06-30 01:28:20 +1000

    Create get_lectures.php

---
+++ get_lectures.php
@@ -0,0 +1,25 @@
+<?php
+// Подключение к базе данных
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "mydb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_error);
+}
+
+$sql = "SELECT * FROM lecture";
+$result = $conn->query($sql);
+
+$lectures = array();
+while($row = $result->fetch_assoc()) {
+    $lectures[] = $row;
+}
+
+echo json_encode($lectures);
+
+$conn->close();
+?>
