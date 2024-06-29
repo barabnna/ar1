3cb2dbe7006c7a93b75b5078f5edea258f02627b
barabnna <154731299+barabnna@users.noreply.github.com>
2024-06-30 01:08:46 +1000

    Add files via upload

---
+++ mybd.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 29 2024 г., 18:07
+-- Версия сервера: 10.3.22-MariaDB
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `mybd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lecture`
+--
+
+CREATE TABLE `lecture` (
+  `id_lecture` int(11) NOT NULL,
+  `title_lecture` varchar(50) NOT NULL,
+  `text_lecture` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `lecture`
+--
+ALTER TABLE `lecture`
+  ADD PRIMARY KEY (`id_lecture`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `lecture`
+--
+ALTER TABLE `lecture`
+  MODIFY `id_lecture` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
